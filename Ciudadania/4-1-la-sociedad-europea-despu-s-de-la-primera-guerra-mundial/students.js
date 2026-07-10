aa3952c8ad9388f745ce590aa98c5c85f4e059bc
coeduca <coeducaanimas@gmail.com>
2026-07-09 19:21:03 -0600

    Last Sync: 2026-07-09 19:21 (Mobile)

---
+++ Ciudadania/4-1-la-sociedad-europea-despu-s-de-la-primera-guerra-mundial/students.js
@@ -0,0 +1,25 @@
+// Base de datos de estudiantes (autogenerada por la app)
+const STUDENTS = {
+
+  // OCTAVO
+  "20176655": { name: "Genesis Guadalupe Angel Martinez", grade: "Octavo" },
+  "20176656": { name: "Karen Melany Barrera Hernández", grade: "Octavo" },
+  "20176657": { name: "Emerson Geremy Barrera Hernández", grade: "Octavo" },
+  "20176661": { name: "Gustavo Ezequiel Corvera Alfaro", grade: "Octavo" },
+  "20138869": { name: "Diego Benjamin Corvera De Paz", grade: "Octavo" },
+  "20176662": { name: "Pedro Ernesto Corvera Flores", grade: "Octavo" },
+  "20176663": { name: "Luis Rafael Corvera Fuentes", grade: "Octavo" },
+  "10065035": { name: "Justin Leonel Duran Gomez", grade: "Octavo" },
+  "20176664": { name: "Hector Gabriel Gonzalez Corvera", grade: "Octavo" },
+  "20305579": { name: "Estiven Alexander Henriquez Chirino", grade: "Octavo" },
+  "20305581": { name: "Keily Yaritza Manueles Pereira", grade: "Octavo" },
+  "20176667": { name: "Gisela Lisbeth Martinez Flores", grade: "Octavo" },
+  "20176668": { name: "Ederson Osiel Martinez Lazo", grade: "Octavo" },
+  "20179890": { name: "Ezequiel Vladimir Mejia Carbajal", grade: "Octavo" },
+  "20179891": { name: "Raul Ernesto Melendez Corvera", grade: "Octavo" },
+  "20293665": { name: "Katherine Alejandra Perez Barahona", grade: "Octavo" },
+  "20305583": { name: "Samuel Josue Rivas Lopez", grade: "Octavo" },
+
+  // PRUEBAS
+  "1999": { name: "José Eliseo Martínez", grade: "Maestro" },
+};
